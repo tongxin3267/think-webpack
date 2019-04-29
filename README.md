03308fe6e942c157b01d894645aaa85c85edfbf4
申法宽 <shenfakuan@163.com>
2019-04-29 15:09:56 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # think-webpack
-Use webpack in thinkphp6
+在 ThinkPHP6中使用 webpack
+
+## 环境需求
+
+- php >= 7.1
+- thinkphp >= 6
+
+## 安装
+
+```
+composer require kuange/think-webpack
+```
+依赖于 `@symfony/webpack-encore`, 将会自动安装, 并在项目根目录生成以下文件
+
+```
+|____
+|____webpack.config.js
+|____assets
+| |____css
+| | |____app.css
+| |____js
+| | |____app.js
+```
+
+## 使用
+
+```
+# 载入 css 文件
+{:encore_link_tags('app')}
+
+# 载入 js 文件
+{:encore_script_tags('app')}
+```
+
+## 参考链接
+
+https://symfony.com/doc/current/frontend/encore/installation.html
+https://symfony.com/doc/current/frontend.html
---
+++ src/tags.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * 生成 tag 标签
+ * @param string $name
+ * @param string $type
+ */
+function encore_tags(string $name, string $type)
+{
+    $entry_points_file = public_path() . 'entrypoints.json';
+    if (file_exists($entry_points_file)) {
+        $data = json_decode(file_get_contents($entry_points_file), true);
+        $js = $data['entrypoints'][$name][$type];
+        foreach ($js as $i) {
+            switch ($type) {
+                case 'css':
+                    echo '<link rel="stylesheet" href="' . $i . '" />' . "\r\n";
+                    break;
+                case 'js':
+                    echo '<script src="' . $i . '"></script>' . "\r\n";
+                    break;
+                default:
+            }
+        }
+    }
+}
+
+/**
+ * link 标签
+ * @param string $name
+ */
+function encore_link_tags(string $name = '')
+{
+    encore_tags($name, 'css');
+}
+
+/**
+ * script 标签
+ * @param string $name
+ */
+function encore_script_tags(string $name = '')
+{
+    encore_tags($name, 'js');
+}
